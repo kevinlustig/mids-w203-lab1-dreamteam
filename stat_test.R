6840fd44dc7d931977774182ed072640ad78326b
Anuradha Passan <annie.passan@gmail.com>
2022-02-25 17:09:04 +0100

    update

---
+++ stat_test.R
@@ -0,0 +1,8 @@
+# STatistical testing plan
+
+# index - Wilcoxon rank sum [current dataset]
+
+# subjective bin - Wilcoxon rank sum [current dataset]
+
+# should make table on the difficulty and not-difficulty by party 
+
